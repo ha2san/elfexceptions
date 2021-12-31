d4f7949320e1587ce95fc25d8b7380d64e88cb4b
habib <hassan.habib@epfl.ch>
2021-12-31 15:52:36 +0100

    first commit

---
+++ elfexceptions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+from elftools.elf.elffile import ELFFile
+from elftools.elf.relocation import RelocationSection
+
+
+def process_file(fname):
+    with open(fname, 'rb') as f:
+        e = ELFFile(f)
+        for section in e.iter_sections():
+            if not isinstance(section, RelocationSection):
+                continue
+            symbol_table = e.get_section(section['sh_link'])
+            for relocation in section.iter_relocations():
+                symbol = symbol_table.get_symbol(relocation['r_info_sym'])
+                if(symbol.name == "__cxa_allocate_exception"):
+                    print("youpi found:",symbol.name)
+                    return True
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        if process_file(sys.argv[1]):
+            print("uses exception")
+        else:
+            print("does not use exception")
