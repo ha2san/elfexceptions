23b36b461dec297674daa6a14bb53d6b7a49c374
habib <hassan.habib@epfl.ch>
2021-12-31 15:59:54 +0100

    look only in plt

--- elfexceptions.py
+++ elfexceptions.py
@@ -8,15 +8,16 @@ from elftools.elf.relocation import RelocationSection
 def process_file(fname):
     with open(fname, 'rb') as f:
         e = ELFFile(f)
-        for section in e.iter_sections():
-            if not isinstance(section, RelocationSection):
-                continue
-            symbol_table = e.get_section(section['sh_link'])
-            for relocation in section.iter_relocations():
-                symbol = symbol_table.get_symbol(relocation['r_info_sym'])
-                if(symbol.name == "__cxa_allocate_exception"):
-                    print("youpi found:",symbol.name)
-                    return True
+        section = e.get_section_by_name(".rela.plt")
+        #for section in e.iter_sections():
+        if not isinstance(section, RelocationSection):
+            return False 
+        symbol_table = e.get_section(section['sh_link'])
+        for relocation in section.iter_relocations():
+            symbol = symbol_table.get_symbol(relocation['r_info_sym'])
+            if(symbol.name == "__cxa_allocate_exception"):
+                print("youpi found:",symbol.name)
+                return True
 
 
 if __name__ == '__main__':
